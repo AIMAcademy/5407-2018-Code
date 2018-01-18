f9b029487be8d43b4ac0c9bd0510158dbdd35815
AIM Robotics <wpr@aimpa.org>
2018-01-18 18:28:51 -0500

    Added CTRE Talons and working arcade drive

--- src/org/usfirst/frc/team5407/robot/Robot.java
+++ src/org/usfirst/frc/team5407/robot/Robot.java
@@ -7,6 +7,9 @@
 
 package org.usfirst.frc.team5407.robot;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
@@ -17,19 +20,31 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
  * it contains the code necessary to operate a robot with tank drive.
  */
 public class Robot extends IterativeRobot {
-	private DifferentialDrive m_myRobot;
+	private DifferentialDrive _drive;
 	private Joystick m_leftStick;
-	private Joystick m_rightStick;
+	
+	/* talons for arcade drive */
+	WPI_TalonSRX _frontLeftMotor = new WPI_TalonSRX(11); 		/* device IDs here (1 of 2) */
+	WPI_TalonSRX _frontRightMotor = new WPI_TalonSRX(15);
+	
+	WPI_TalonSRX _backLeftSlave = new WPI_TalonSRX(12);
+	WPI_TalonSRX _backRightSlave = new WPI_TalonSRX(16);
+
 
 	@Override
 	public void robotInit() {
-		m_myRobot = new DifferentialDrive(new Spark(0), new Spark(1));
+		_backLeftSlave.follow(_frontLeftMotor);
+		_backRightSlave.follow(_frontRightMotor);
+		
+		_drive = new DifferentialDrive(_frontLeftMotor, _frontRightMotor);
 		m_leftStick = new Joystick(0);
-		m_rightStick = new Joystick(1);
 	}
 
 	@Override
 	public void teleopPeriodic() {
-		m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
+		// Arcade Drive
+    	double forward = -m_leftStick.getY(); // logitech gampad left X, positive is forward
+    	double turn = m_leftStick.getX(); // logitech gampad right X, positive means turn right
+    	_drive.arcadeDrive(forward, turn);
 	}
 }
