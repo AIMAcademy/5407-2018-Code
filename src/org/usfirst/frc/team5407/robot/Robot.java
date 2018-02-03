26866a46d15499687ee88299383e6b556d38c0b6
AIM Robotics <wpr@aimpa.org>
2018-02-03 13:53:33 -0500

    Removed drive code

--- src/org/usfirst/frc/team5407/robot/Robot.java
+++ src/org/usfirst/frc/team5407/robot/Robot.java
@@ -7,36 +7,23 @@
 
 package org.usfirst.frc.team5407.robot;
 
-
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.cscore.VideoMode;
 import edu.wpi.cscore.VideoMode.PixelFormat;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SerialPort;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 /**
  * This is a demo program showing the use of the RobotDrive class, specifically
  * it contains the code necessary to operate a robot with tank drive.
  */
 public class Robot extends IterativeRobot {
-	private DifferentialDrive _drive;
-	private Joystick m_leftStick;
 	
 	// JeVois Variables
 	private SerialPort jevois = null;
 	private int loopCount;
 	private UsbCamera jevoisCam;
-	
-	/* talons for arcade drive */
-	WPI_TalonSRX _frontLeftMotor = new WPI_TalonSRX(11); 		
-	WPI_TalonSRX _frontRightMotor = new WPI_TalonSRX(15);
-	
-	WPI_TalonSRX _backLeftSlave = new WPI_TalonSRX(12);
-	WPI_TalonSRX _backRightSlave = new WPI_TalonSRX(16);
 	
 	private ICameraSettings _currentCameraSettings = new DumbCameraSettings();
 
@@ -46,13 +33,8 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void robotInit() {
-		_backLeftSlave.follow(_frontLeftMotor);
-		_backRightSlave.follow(_frontRightMotor);
-		
-		_drive = new DifferentialDrive(_frontLeftMotor, _frontRightMotor);
-		m_leftStick = new Joystick(0);
-		
-    	// BEGIN JeVois Code //
+
+	// BEGIN JeVois Code //
 		
 	// Tries to reach camera camera and if not, it prints out a failed 
 	// Without this if it did not connect, the whole program would crash
@@ -130,15 +112,6 @@ public class Robot extends IterativeRobot {
 		int bytes = jevois.writeString(cmd);
 		System.out.println("wrote " +  bytes + "/" + cmd.length() + " bytes");	
 		loopCount = 0;
-	}
-
-	@Override
-	public void teleopPeriodic() {
-		// Arcade Drive, uses one joystick and takes the x and y values of the joystick 
-	    	double forward = -m_leftStick.getY(); // logitech gampad left X, positive is forward
-	    	double turn = m_leftStick.getX(); // logitech gampad right X, positive means turn right
-	    	_drive.arcadeDrive(forward, turn);
-		checkJeVois();
 	}
 
 	// Private camera settings code
